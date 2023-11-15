4e1fa1391ccd06a347e54bf41b0548f8f505a31e
Joao-Gimenes <joaogimenes9@gmail.com>
2023-11-15 14:00:48 -0300

    Adicionado requisições de Usuario assim como no medicine, e corrigido erro do override doPut em sala de aula

--- MedGen/src/java/web/ApiServlet.java
+++ MedGen/src/java/web/ApiServlet.java
@@ -8,8 +8,12 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import model.Medicine;
 import org.json.JSONObject;
 import model.Users;
+import org.json.JSONArray;
 
 @WebServlet(name = "ApiServlet", urlPatterns = {"/api/*"})
 public class ApiServlet extends HttpServlet {
@@ -32,6 +36,8 @@ public class ApiServlet extends HttpServlet {
                 processSession(file, request, response);
             } else if(request.getRequestURI().endsWith("/api/users")){
                 processUsers(file, request, response);
+            } else if(request.getRequestURI().endsWith("/api/medicine")){
+                processMedicine(file, request, response);
             }
         } catch(Exception ex){
             response.sendError(500,"Internal Error: "+ex.getLocalizedMessage());
@@ -53,13 +59,13 @@ public class ApiServlet extends HttpServlet {
     @Override
     protected void doDelete(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+        processRequest(request, response);
     }
 
     @Override
     protected void doPut(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+        processRequest(request, response);
     }
     
     @Override
@@ -70,9 +76,6 @@ public class ApiServlet extends HttpServlet {
     // Processar a sessão do usuario
     private void processSession(JSONObject file, HttpServletRequest request, HttpServletResponse response) throws Exception {
         if(request.getMethod().toLowerCase().equals("put")){
-            // teste String body = getJSONBODY(request.getReader()).toString();
-            // teste String login = new JSONObject(body).getString("login");
-            // teste String password = new JSONObject(body).getString("password");
             JSONObject body = getJSONBODY(request.getReader());
             String login = body.getString("login");
             String password = body.getString("password");
@@ -112,7 +115,65 @@ public class ApiServlet extends HttpServlet {
     }
 
     private void processUsers(JSONObject file, HttpServletRequest request, HttpServletResponse response) throws Exception {
-        
+        if(request.getSession().getAttribute("users")== null){
+            response.sendError(401,"Unauthorized: No session");
+        } else if(!((Users)request.getSession().getAttribute("users")).getRole().equals("ADMIN")){
+            response.sendError(401,"Unauthorized: Only admin can manage users");
+        } else if(request.getMethod().toLowerCase().equals("get")){
+            file.put("list", new JSONArray(Users.getUsers()));
+        } else if(request.getMethod().toLowerCase().equals("post")){
+            JSONObject body = getJSONBODY(request.getReader());
+            String login = body.getString("login");
+            String name = body.getString("name");
+            String role = body.getString("role");
+            String password = body.getString("password");
+            Users.insertUser(login, name, role, password);
+        } else if(request.getMethod().toLowerCase().equals("put")){
+            JSONObject body = getJSONBODY(request.getReader());
+            String login = body.getString("login");
+            String name = body.getString("name");
+            String role = body.getString("role");
+            String password = body.getString("password");
+            Users.updateUser(login, name, role, password);
+        } else if(request.getMethod().toLowerCase().equals("delete")){
+            Long id = Long.parseLong(request.getParameter("id"));
+            Users.deleteUser(id);
+        } else{
+            response.sendError(405,"Methodo not allowed");
+        }
+    }
+
+    private void processMedicine(JSONObject file, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        if(request.getSession().getAttribute("users")== null){
+            response.sendError(401,"Unauthorized: No session");
+        } else if(request.getMethod().toLowerCase().equals("get")){
+            file.put("list", new JSONArray(Medicine.getMedicines()));
+        } else if(request.getMethod().toLowerCase().equals("post")){
+            JSONObject body = getJSONBODY(request.getReader());
+            String name = body.getString("name");
+            String category = body.getString("category");
+            int quantity = body.getInt("quantity");
+            double price = body.getDouble("price");
+            String sdate = body.getString("date");
+            SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+            Date date = formatter.parse(sdate);
+            Medicine.insertMedicine(name, category, quantity, price, date);
+        } else if(request.getMethod().toLowerCase().equals("put")){
+            JSONObject body = getJSONBODY(request.getReader());
+            String name = body.getString("name");
+            String category = body.getString("category");
+            int quantity = body.getInt("quantity");
+            double price = body.getDouble("price");
+            String sdate = body.getString("date");
+            SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+            Date date = formatter.parse(sdate);
+            Medicine.updateMedicine(name, category, quantity, price, date);
+        } else if(request.getMethod().toLowerCase().equals("delete")){
+            Long id = Long.parseLong(request.getParameter("id"));
+            Medicine.deleteMedicine(id);
+        } else{
+            response.sendError(405,"Methodo not allowed");
+        }
     }
 
 }
